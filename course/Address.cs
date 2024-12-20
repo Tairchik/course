ae20b89e02c0f4511dc0a6e3ac0a849dbc15b5ad
Tair <gidrolaz.tr@gmail.com>
2024-12-20 15:43:13 +0700

    Добил класс address и общий класс person

---
+++ course/Address.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course
+{
+    internal class Address
+    {
+        private string country;
+        private string region;
+        private string locality;
+        private string street;
+        private string construction;
+
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                if (string.IsNullOrEmpty(country))
+                {
+                    throw new ArgumentNullException("Поле <страна> не может быть пустой");
+                }
+                country = value;
+            }
+        }
+        public string Region
+        {
+            get { return region; }
+            set
+            {
+                if (string.IsNullOrEmpty(region))
+                {
+                    throw new ArgumentNullException("Поле <страна> не может быть пустой");
+                }
+                region = value;
+            }
+        }
+        public string Locality
+        {
+            get { return locality; }
+            set
+            {
+                if (string.IsNullOrEmpty(locality))
+                {
+                    throw new ArgumentNullException("Поле <страна> не может быть пустой");
+                }
+                locality = value;
+            }
+        }
+        public string Street
+        {
+            get
+            {
+                return street;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(street))
+                {
+                    throw new ArgumentNullException("Поле <страна> не может быть пустой");
+                }
+                street = value;
+            }
+        }
+        public string Construction
+        {
+            get
+            {
+                return construction;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(construction))
+                {
+                    throw new ArgumentNullException("Поле <страна> не может быть пустой");
+                }
+                construction = value;
+            }
+        }
+
+        public Address (string country, string region, string locality, string street, string construction) 
+        {
+            Country = country;
+            Region = region;
+            Locality = locality;
+            Street = street;
+            Construction = construction;
+        }
+    }
+}
--- course/Program.cs
+++ course/Program.cs
@@ -11,6 +11,38 @@ namespace course
         static void Main(string[] args)
         {
             Console.WriteLine();
+            Post maneger = new Post() {namePost = "менеджер", salary = 12000};
+            maneger.salary = 15000;
+            Worker w = new Worker("Иванов");
+            Worker a = new Worker("Petr", maneger);
+
+        }
+        public class Worker
+        {
+            private string fio;
+            private Post post;
+
+            public string FIO 
+            {
+                get { return fio;}
+                set { fio = value; }
+            }
+            public Worker(string fio, Post post)
+            {
+                this.post = post;
+                FIO = fio;
+            }
+            public Worker(string fio)
+            {
+                FIO = fio;
+            }
+        }
+        public class Post
+        {
+            public string namePost { get; set; }
+            public int salary { get; set; }
+
+           
         }
     }
 }
