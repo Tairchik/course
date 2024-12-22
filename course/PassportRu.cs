cba2d2df5f9f4d0e2bbda3ad0980dc0e9b6ce02a
Tair <gidrolaz.tr@gmail.com>
2024-12-22 17:39:15 +0700

    Добавлен паспорт российского образца PassportRU, как реализация интерфейса IPassport

---
+++ course/PassportRu.cs
@@ -0,0 +1,51 @@
+﻿using course.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course
+{
+    internal class PassportRu : IPassport
+    {
+        private string series;
+        private string number;
+
+        public PassportRu(string series, string number)
+        {
+            Series = series;
+            Number = number;
+        }
+        public string Series 
+        {  
+            get { return series; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value) || value.Length < 4) 
+                {
+                    throw new ArgumentException("Неверная серия паспорта");
+                }
+                else
+                {
+                    series = value;
+                }
+            }
+        }
+        public string Number 
+        { 
+            get { return number; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value) || value.Length < 8)
+                {
+                    throw new ArgumentException("Неверный номер паспорта");
+                }
+                else
+                {
+                    number = value;
+                }
+            } 
+        }
+    }
+}
