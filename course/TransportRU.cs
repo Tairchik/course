b68ff50ca6aaf3cf9f49dc1071229e178425da98
Tair <gidrolaz.tr@gmail.com>
2024-12-22 16:54:45 +0700

    Изменил логику классу траспорт

--- course/TransportRU.cs
+++ course/TransportRU.cs
@@ -3,17 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace course
 {
-    internal class Transport : ITransport
+    internal class TransportRU : ITransport
     {
 
         private string registerNumber;
         private string carName;
+        string patternRUCar = @"^[АВЕКМНОРСТУХ]{1}[0-9]{3}[АВЕКМНОРСТУХ]{2}[- ]?[0-9]{2,3}$";
+        string patternEACar = @"^[ABEKMHOPCTYX]{1}[0-9]{3}[ABEKMHOPCTYX]{2}[- ]?[0-9]{2,3}$";
+        string patternRUMotorcycle = @"^[АВЕКМНОРСТУХ]{2}[0-9]{4}[- ]?[0-9]{2,3}$";
+        string patternEAMotorcycle = @"^[ABEKMHOPCTYX]{2}[0-9]{4}[- ]?[0-9]{2,3}$";
 
-        public Transport(string registerNumber, string carName)
+
+
+
+        public TransportRU(string registerNumber, string carName)
         {
             CarName = carName;
             RegisterNumber = registerNumber;
@@ -46,6 +54,11 @@ namespace course
             set
             {
                 if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Неверный регистрационный номер транспорта");
+                }
+                else if (!Regex.IsMatch(value.ToUpper(), patternRUCar) && !Regex.IsMatch(value.ToUpper(), patternEACar) 
+                    && !Regex.IsMatch(value.ToUpper(), patternEAMotorcycle) && !Regex.IsMatch(value.ToUpper(), patternRUMotorcycle))
                 {
                     throw new ArgumentException("Неверный регистрационный номер транспорта");
                 }
