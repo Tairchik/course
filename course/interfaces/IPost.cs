b43b6f4179e2fd8f2a10faaeb6023aa6c0bd244b
Tair <gidrolaz.tr@gmail.com>
2024-12-22 12:29:11 +0700

    Добавлен интерфейс работинк: IWorker. И входящие в него классы: IPassport(паспорт: серия и номер); IPensionFund(пенсионный фонд); IPost (Должность работника и зп по должности) Iinn (инн работника) ITransport (Рабочий транспорт закрепленный за работником)

---
+++ course/interfaces/IPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course.interfaces
+{
+    internal interface IPost
+    {
+        Decimal Salary { get; set; }
+        string namePost { get; set; }
+    }
+}
---
+++ course/interfaces/IWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course.interfaces
+{
+    internal interface IWorker
+    {
+        string Name { get; set; }
+        string Surname { get; set; }
+        string Patronymic { get; set; }
+        IPost PostData  { get; set; }
+        Decimal BonusSalary { get; set; }
+        ITransport TransportData { get; set; }
+        IPassport PassportData { get; set; }
+        Iinn InnData { get; set; }
+        IPensionFund PensionFundData { get; set; }
+
+    }
+}
